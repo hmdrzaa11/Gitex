886a3c7862ec11bdd706e856c5bf1f6d024c9598
Hamid Oujand <hmdrzaa11@gmail.com>
2022-01-05 01:15:18 +0330

    added a basic express server

---
+++ auth/src/index.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import { json } from "body-parser";
+
+let app = express();
+
+app.use(json());
+
+app.listen(3000, () => {
+  console.log("Auth listening on 3000");
+});
