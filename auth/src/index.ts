9e1844a1ce0144e422dd6faad50283d0e24cdfb3
Hamid Oujand <hmdrzaa11@gmail.com>
2022-01-05 02:58:50 +0330

    added ingress-srv file

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,10 @@ let app = express();
 
 app.use(json());
 
+app.get("/api/users/currentuser", (req, res) => {
+  res.send({ msg: "Hi There" });
+});
+
 app.listen(3000, () => {
-  console.log("Auth listening on 3000");
+  console.log("Auth listening on 3000!!!");
 });
