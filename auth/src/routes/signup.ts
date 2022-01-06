0631ecec720369d0448a395a07cba8931aca42ba
Hamid Oujand <hmdrzaa11@gmail.com>
2022-01-06 18:58:53 +0330

    add jwt token generation

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import jwt from "jsonwebtoken";
+
 import { RequestValidationError } from "../errors/request-validation-error";
 import { BadRequestError } from "../errors/bad-request-error";
 import { User } from "../models/user";
@@ -31,6 +33,19 @@ router.post(
 
     let user = User.build({ email, password });
     await user.save();
+    //Generate the JWT
+    let userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      "asdf"
+    );
+    // and store it on session obj
+    req.session = {
+      jwt: userJwt,
+    };
+
     res.status(201).send(user);
   }
 );
