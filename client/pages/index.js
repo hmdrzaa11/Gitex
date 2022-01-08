c6528f477be8b2081d8186e1553b0c6a28192160
Hamid Oujand <hmdrzaa11@gmail.com>
2022-01-09 00:42:59 +0330

    add pages and bootstrap

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,15 @@
-export default () => {
-  return <h1>Landing page</h1>;
+import axios from "axios";
+
+let LandingPage = ({ currentUser }) => {
+  // console.log(currentUser);
+  return <h1>Landing page </h1>;
 };
+
+LandingPage.getInitialProps = async () => {
+  // let response = await axios.get("/api/users/currentuser");
+  console.log("I was executed ....");
+  // return response.data;
+  return {};
+};
+
+export default LandingPage;
