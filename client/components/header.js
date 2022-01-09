598d1ad076739bc3679a756b94ccd0a9ebbf54a3
Hamid Oujand <hmdrzaa11@gmail.com>
2022-01-09 20:17:05 +0330

    add getInitialProps to the _app.js

---
+++ client/components/header.js
@@ -0,0 +1,36 @@
+import Link from "next/link";
+
+export default ({ currentUser }) => {
+  let links = [
+    !currentUser && {
+      label: "Sign Up",
+      href: "/auth/signup",
+    },
+    !currentUser && {
+      label: "Sign In",
+      href: "/auth/signin",
+    },
+    currentUser && {
+      label: "Sign Out",
+      href: "/auth/signout",
+    },
+  ]
+    .filter((linkConf) => linkConf)
+    .map(({ label, href }) => (
+      <li className="nav-item" key={href}>
+        <Link href={href}>
+          <a className="nav-link">{label}</a>
+        </Link>
+      </li>
+    ));
+  return (
+    <nav className="navbar navbar-light bg-light ">
+      <Link href="/">
+        <a className="navbar-brand">Gitix</a>
+      </Link>
+      <div className="d-flex justify-content-end">
+        <ul className="nav d-flex align-items-center">{links}</ul>
+      </div>
+    </nav>
+  );
+};
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,29 @@
 import "bootstrap/dist/css/bootstrap.css";
+import buildClient from "../api/buildClient";
+import Header from "../components/header";
 
-export default ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+let AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <Header currentUser={currentUser} />
+      <Component {...pageProps} />
+    </div>
+  );
 };
+
+AppComponent.getInitialProps = async (appContext) => {
+  let client = buildClient(appContext.ctx);
+  let { data } = await client.get("/api/users/currentuser");
+
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx); //it needs to be passed the "Ctx" object
+  }
+
+  return {
+    pageProps,
+    ...data,
+  };
+};
+
+export default AppComponent;
