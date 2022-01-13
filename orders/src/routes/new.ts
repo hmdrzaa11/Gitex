97de4324885fcde4053a8ade6f01dece40583c5d
Hamid Oujand <hmdrzaa11@gmail.com>
2022-01-14 01:18:32 +0330

    install updated version of common module inside of orders service

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,25 @@
+import mongoose from "mongoose";
 import express, { Request, Response } from "express";
+import { requireAuth, validateRequest } from "@hamidtickets/common";
+import { body } from "express-validator";
 
 let router = express.Router();
 
-router.post("/api/orders", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.post(
+  "/api/orders",
+  requireAuth,
+  [
+    body("titleId")
+      .notEmpty()
+      .withMessage("ticketId must be defined")
+      .custom((input: string) => {
+        return mongoose.isValidObjectId(input);
+      }),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    res.send({});
+  }
+);
 
 export { router as newOrderRouter };
