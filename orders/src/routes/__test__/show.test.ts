972dc3da0d47763b19d39ae2b9bef9cb9d5d1f1a
Hamid Oujand <hmdrzaa11@gmail.com>
2022-01-14 20:47:07 +0330

    added show route for orders and also test

---
+++ orders/src/routes/__test__/show.test.ts
@@ -0,0 +1,42 @@
+import request from "supertest";
+import { app } from "../../app";
+import { Ticket } from "../../models/ticket";
+
+it("fetches the order", async () => {
+  //create a ticket
+  let ticket = Ticket.build({ title: "concert", price: 20 });
+  await ticket.save();
+  //make a request to create an order
+  let user = signin();
+  let { body: order } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+  //make a request to fetch the order
+  let repsonse = await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set("Cookie", user)
+    .send()
+    .expect(200);
+  expect(repsonse.body.id).toEqual(order.id);
+});
+
+it("returns an error if one user tries to fetch another users order", async () => {
+  //create a ticket
+  let ticket = Ticket.build({ title: "concert", price: 20 });
+  await ticket.save();
+  //make a request to create an order
+  let user = signin();
+  let { body: order } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+  //make a request to fetch the order
+  let repsonse = await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set("Cookie", signin())
+    .send()
+    .expect(401);
+});
--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,9 +1,22 @@
 import express, { Request, Response } from "express";
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth,
+} from "@hamidtickets/common";
+import { Order } from "../models/order";
 
 let router = express.Router();
 
-router.get("/api/orders/:orderId", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.get(
+  "/api/orders/:orderId",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    let order = await Order.findById(req.params.orderId).populate("ticket");
+    if (!order) throw new NotFoundError();
+    if (order.userId !== req.currentUser!.id) throw new NotAuthorizedError();
+    res.send(order);
+  }
+);
 
 export { router as showOrderRouter };
