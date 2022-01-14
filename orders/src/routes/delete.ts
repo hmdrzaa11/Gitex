657712bb186c437d33f382c7f3e9df6147492589
Hamid Oujand <hmdrzaa11@gmail.com>
2022-01-14 22:17:18 +0330

    added delete route for orders and also test

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,25 @@
 import express, { Request, Response } from "express";
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth,
+} from "@hamidtickets/common";
+import { Order, OrderStatus } from "../models/order";
 
 let router = express.Router();
 
-router.delete("/api/orders/:orderId", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.delete(
+  "/api/orders/:orderId",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    let { orderId } = req.params;
+    let order = await Order.findById(orderId);
+    if (!order) throw new NotFoundError();
+    if (order.userId !== req.currentUser!.id) throw new NotAuthorizedError();
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+    res.status(204).send(order);
+  }
+);
 
 export { router as deleteOrderRouter };
