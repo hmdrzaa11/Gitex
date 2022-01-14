40a7541f8a4e22c570d2cf1c889f00dcd74cb0e1
Hamid Oujand <hmdrzaa11@gmail.com>
2022-01-14 20:46:14 +0330

    added index route for orders and also test

---
+++ orders/src/routes/__test__/index.test.ts
@@ -0,0 +1,45 @@
+import request from "supertest";
+import { app } from "../../app";
+import { Order } from "../../models/order";
+import { Ticket } from "../../models/ticket";
+
+let createTicket = async () => {
+  let ticket = Ticket.build({ title: "concert", price: 20 });
+  await ticket.save();
+  return ticket;
+};
+
+it("fetches orders for particular user ", async () => {
+  //create 3 tickets
+  let ticketOne = await createTicket();
+  let ticketTwo = await createTicket();
+  let ticketThree = await createTicket();
+  let userOne = signin();
+  let userTwo = signin();
+  //create 1 order as user number 1
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", userOne)
+    .send({ ticketId: ticketOne.id })
+    .expect(201);
+  //create 2 orders as user number 2
+  let { body: orderOne } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", userTwo)
+    .send({ ticketId: ticketTwo.id })
+    .expect(201);
+  let { body: orderTwo } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", userTwo)
+    .send({ ticketId: ticketThree.id })
+    .expect(201);
+  //make request to get orders for user number 2
+  let response = await request(app)
+    .get("/api/orders")
+    .set("Cookie", userTwo)
+    .send()
+    .expect(200);
+  expect(response.body.length).toEqual(2);
+  expect(response.body[0].id).toEqual(orderOne.id);
+  expect(response.body[1].id).toEqual(orderTwo.id);
+});
