564fec6e0e91cfc684b02314aa67b802aa9a822d
Hamid Oujand <hmdrzaa11@gmail.com>
2022-01-16 19:51:29 +0330

    added a ticket-updated-publisher to the order-created-listener

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -1,6 +1,7 @@
 import { Listener, OrderCreatedEvent, Subjects } from "@hamidtickets/common";
 import { Message } from "node-nats-streaming";
 import { queueGroupName } from "./queue-group-name";
+import { TicketUpdatedPublisher } from "../publishers/ticket-updated-publisher";
 
 import { Ticket } from "../../models/ticket";
 
@@ -21,6 +22,15 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
     //save the ticket
     await ticket.save();
 
+    //publish an event when
+    await new TicketUpdatedPublisher(this.client).publish({
+      id: ticket.id,
+      version: ticket.version,
+      price: ticket.price,
+      title: ticket.title,
+      userId: ticket.userId,
+      orderId: ticket.orderId,
+    });
     //ack the message
     msg.ack();
   }
