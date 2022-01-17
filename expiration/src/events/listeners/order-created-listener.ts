3c62403ef3d7c5b8f7b790b77a0dd2b7d0a88ff4
Hamid Oujand <hmdrzaa11@gmail.com>
2022-01-17 19:15:04 +0330

    added bull-queue and dely to it

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -13,7 +13,13 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   queueGroupName: string = queueGroupName;
 
   async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
-    await expirationQueue.add({ orderId: data.id });
+    let delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+    await expirationQueue.add(
+      { orderId: data.id },
+      {
+        delay: delay,
+      }
+    );
     msg.ack();
   }
 }
