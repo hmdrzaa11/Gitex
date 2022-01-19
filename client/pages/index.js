10b024778cb477323c200985771bbf54c5970b7c
Hamid Oujand <hmdrzaa11@gmail.com>
2022-01-20 00:56:36 +0330

    refactor landing page to show list of tickets

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,13 +1,31 @@
-let LandingPage = ({ currentUser }) => {
-  return currentUser ? (
-    <h1>You are signed in</h1>
-  ) : (
-    <h1>You're not signed in</h1>
+let LandingPage = ({ currentUser, tickets }) => {
+  let ticketList = tickets.map((ticket) => (
+    <tr key={ticket.id}>
+      <td>{ticket.title}</td>
+      <td>{ticket.price}</td>
+    </tr>
+  ));
+  return (
+    <div>
+      <h1>Tickets</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>{ticketList}</tbody>
+      </table>
+    </div>
   );
 };
 
-LandingPage.getInitialProps = async (context) => {
-  return {};
+LandingPage.getInitialProps = async (context, client, currentUser) => {
+  let { data } = await client.get("/api/tickets");
+  return {
+    tickets: data,
+  };
 };
 
 export default LandingPage;
